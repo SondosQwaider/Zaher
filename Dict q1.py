ea4cc913a514425b9086e882647fe482bf42bbba
Sodnos <rrttyyuuiioo2000@gmail.com>
2022-10-06 13:35:07 +0300

    Ok

---
+++ Dict q1.py
@@ -0,0 +1,4 @@
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+dictionary = {k: v for k, v in zip(keys, values)}
+print(dictionary)
---
+++ Sets q1.py
@@ -0,0 +1,10 @@
+sample_set = {"Yellow", "Orange", "Black"}
+# a list of numbers
+list_of_num = ["Blue", "Green", "Red"]
+
+# add all elements in list to the set
+sample_set.update(list_of_num)
+print('Modified Set: ')
+print(sample_set)
+
+
